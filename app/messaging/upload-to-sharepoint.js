19e872502b9f1487ee74e3d569874d375dcf15ff
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-03-22 20:59:20 +0000

    Download file from blob store

--- app/messaging/upload-to-sharepoint.js
+++ app/messaging/upload-to-sharepoint.js
@@ -1,8 +1,19 @@
+const { BlobServiceClient } = require('@azure/storage-blob')
+
 module.exports = async function (msg, fileCreatedReceiver) {
   try {
     const { body } = msg
     console.log('Received message:')
     console.log(body)
+
+    const connStr = process.env.BLOB_STORAGE_CONNECTION_STRING
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connStr)
+    const containerClient = blobServiceClient.getContainerClient('paul-test')
+    const blockBlobClient = containerClient.getBlockBlobClient(body.filename)
+    const result = await blockBlobClient.downloadToFile(body.filename)
+    console.log(result)
+    console.log('Downloaded successfully')
+
     await fileCreatedReceiver.completeMessage(msg)
   } catch (err) {
     console.error('Unable to process message')
