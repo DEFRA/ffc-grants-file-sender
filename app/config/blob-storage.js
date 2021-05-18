911070a5cd6198def2c8b385410910efe48b16fc
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-05-18 14:15:48 +0100

    Update tests

--- app/config/blob-storage.js
+++ app/config/blob-storage.js
@@ -2,23 +2,19 @@ const Joi = require('joi')
 
 // Define config schema
 const schema = Joi.object({
-  env: Joi.string().valid('development', 'test', 'production').default('development'),
-  connectionStr: Joi.string().when('env', { is: 'production', then: Joi.allow('').optional(), otherwise: Joi.required() }),
-  storageAccountName: Joi.string().when('env', { is: 'production', then: Joi.required(), otherwise: Joi.allow('').optional() }),
+  connectionStr: Joi.string().when('useConnectionStr', { is: true, then: Joi.required(), otherwise: Joi.allow('').optional() }),
+  storageAccountName: Joi.string().when('useConnectionStr', { is: true, then: Joi.allow('').optional(), otherwise: Joi.required() }),
   containerName: Joi.string().required(),
   useConnectionStr: Joi.boolean().default(false)
 })
 
 // Build config
 const config = {
-  env: process.env.NODE_ENV,
   connectionStr: process.env.BLOB_STORAGE_CONNECTION_STRING,
   storageAccountName: process.env.BLOB_STORAGE_ACCOUNT_NAME,
   containerName: process.env.BLOB_STORAGE_CONTAINER_NAME,
   useConnectionStr: process.env.USE_BLOB_STORAGE_CONNECTION_STRING
 }
-
-console.log(config)
 
 // Validate config
 const result = schema.validate(config, {
--- test/integration/narrow/app/config/blob-storage.test.js
+++ test/integration/narrow/app/config/blob-storage.test.js
@@ -11,22 +11,37 @@ describe('Config blobStorage', () => {
   })
 
   test('Valid env vars pass validation and are set', () => {
-    const mockconnectionStr = 'mock-connectionStr'
-    const mockcontainerName = 'mock-containerName'
-    process.env.BLOB_STORAGE_CONNECTION_STRING = mockconnectionStr
-    process.env.BLOB_STORAGE_CONTAINER_NAME = mockcontainerName
+    const mockConnectionStr = 'mock-connectionStr'
+    const mockContainerName = 'mock-containerName'
+    const mockUseConnectionStr = 'true'
+    const mockStorageAccountName = 'mock-storageAccountName'
+
+    process.env.BLOB_STORAGE_CONNECTION_STRING = mockConnectionStr
+    process.env.BLOB_STORAGE_CONTAINER_NAME = mockContainerName
+    process.env.USE_BLOB_STORAGE_CONNECTION_STRING = mockUseConnectionStr
+    process.env.BLOB_STORAGE_ACCOUNT_NAME = mockStorageAccountName
+
     const spreadsheetConfig = require('../../../../../app/config/blob-storage')
-    expect(spreadsheetConfig.connectionStr).toBe(mockconnectionStr)
-    expect(spreadsheetConfig.containerName).toBe(mockcontainerName)
+    expect(spreadsheetConfig.connectionStr).toBe(mockConnectionStr)
+    expect(spreadsheetConfig.containerName).toBe(mockContainerName)
+    expect(spreadsheetConfig.storageAccountName).toBe(mockStorageAccountName)
+    expect(spreadsheetConfig.useConnectionStr).toBe(true)
   })
 
-  test('Connection String undefined throws error', () => {
+  test('Connection String undefined throws error if using connection string', () => {
     process.env.BLOB_STORAGE_CONNECTION_STRING = undefined
+    process.env.USE_BLOB_STORAGE_CONNECTION_STRING = true
     expect(() => require('../../../../../app/config/blob-storage')).toThrow()
   })
 
-  test('Connection Name undefined throws error', () => {
+  test('Container Name undefined throws error', () => {
     process.env.BLOB_STORAGE_CONTAINER_NAME = undefined
+    expect(() => require('../../../../../app/config/blob-storage')).toThrow()
+  })
+
+  test('Storage account name undefined throws error when not using connection string', () => {
+    process.env.BLOB_STORAGE_ACCOUNT_NAME = undefined
+    process.env.USE_BLOB_STORAGE_CONNECTION_STRING = false
     expect(() => require('../../../../../app/config/blob-storage')).toThrow()
   })
 })
