54f051580988cd6de9df88a3474f84e11c8bf5e2
diya2214 <madihakhn@yahoo.com>
2022-11-07 18:59:49 +0000

    test

--- test/integration/narrow/app/config/messaging.test.js
+++ test/integration/narrow/app/config/messaging.test.js
@@ -2,7 +2,7 @@ describe('Config Messaging', () => {
   const messageConfig = require('../../../../../app/config/messaging')
   test('Check queue and topic subscription defined', () => {
     expect(messageConfig).toBeDefined()
-    //expect(messageConfig.fileCreatedSubscription).toBeDefined()
+    expect(messageConfig.fileCreatedSubscription).toBeDefined()
     expect(messageConfig.msgSrc).toBe('ffc-grants-file-sender')
   })
 })
